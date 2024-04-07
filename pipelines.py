4701bc603095caee65500afe1315e60dc42566c2
Dmitry Molchanov <109847524+wbmon@users.noreply.github.com>
2024-04-07 06:23:13 +0300

    Add files via upload

--- pipelines.py
+++ pipelines.py
@@ -3,9 +3,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
 
 import json
 from itemadapter import ItemAdapter
